c5d9e2a37246239e842cf38e4cc8972a6276bf27
willddinh <willddinh@gmail.com>
2024-10-08 09:49:36 +0700

    lesson 113

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+'use client';
+
+export default function Error() {
+    return <main className="error">
+        <h1>An Error occured</h1>
+        <p>Failed to fetch meal data, please try again later.</p>
+    </main>
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals}/>
+}
 
+export default function MealsPage() {
   return (
   <>
   <header className={classes.header}>
@@ -16,7 +20,12 @@ export default async function MealsPage() {
     </p>
   </header>
   <main className={classes.main}>
-      <MealsGrid meals={meals}/>
+      <Suspense fallback={<p className={classes.loading}>
+        Loading Meals...
+    </p>}>
+        <Meals/>
+    </Suspense>
+      
   </main>
 </>
   )
